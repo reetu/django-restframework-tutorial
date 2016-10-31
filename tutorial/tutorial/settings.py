1196ee57dd522bae9e7e145f6ef0cfcb5a7d1138
Reetu Mutti <reetu@openee.io>
2016-10-31 15:34:13 -0700

    Pagination support is too easy. It can't be this easy, surely.

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -70,7 +70,9 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'tutorial.wsgi.application'
-
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 10
+}
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
