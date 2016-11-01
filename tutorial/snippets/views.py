a064c1a011457c23e2804b41dd64bff94fd28857
Reetu Mutti <reetu@openee.io>
2016-11-01 12:30:32 -0700

    Typo in SnippetViewSet - highlighted property misspelt

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -28,7 +28,7 @@ class SnippetViewSet(viewsets.ModelViewSet):
     @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
     def highlight(self, request, *args, **kwargs):
         snippet = self.get_object()
-        return Response(snippet.hightlighted)
+        return Response(snippet.highlighted)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
